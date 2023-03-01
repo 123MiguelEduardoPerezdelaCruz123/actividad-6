f6efc9b26c75836c295d3bb5d801e41a3b42d62c
Miguel Perez <migueledperezdelacruz452@gmail.com>
2023-02-28 23:53:17 -0400

    tarea 1

---
+++ actividad 6/tarea1.js
@@ -0,0 +1,17 @@
+let tarifa_hora=Number(prompt("escribe la tarifa por hora"));
+let dias=Number(prompt("escribe cuántos días trabajará"));
+const un_dia = 8;
+let aldia = dias * un_dia 
+let tarifa = aldia * tarifa_hora
+let tarifa_al_dia = tarifa / dias
+
+function showMessage() {
+    let tarifa_hora=Number(prompt("escribe la tarifa por hora"));
+let dias=Number(prompt("escribe cuántos días trabajará"));
+const un_dia = 8;
+let aldia = dias * un_dia;
+let tarifa = aldia * tarifa_hora;
+let tarifa_al_dia = tarifa / dias;
+}
+
+alert(tarifa_al_dia);
